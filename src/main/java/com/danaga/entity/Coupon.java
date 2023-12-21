986b674d1e68f96d4f15bf1179fba0f066e74fdb
이동현 <133841274+leedong617@users.noreply.github.com>
2023-12-21 23:21:39 +0900

    Update Coupon.java

--- src/main/java/com/danaga/entity/Coupon.java
+++ src/main/java/com/danaga/entity/Coupon.java
@@ -24,7 +24,7 @@ import lombok.ToString;
 public class Coupon extends BaseEntity{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
