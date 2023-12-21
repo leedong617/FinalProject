b617954fac4ece2a80f37644156d33c0d465ccd0
이동현 <133841274+leedong617@users.noreply.github.com>
2023-12-21 23:22:34 +0900

    Update OrderItem.java

--- src/main/java/com/danaga/entity/OrderItem.java
+++ src/main/java/com/danaga/entity/OrderItem.java
@@ -20,8 +20,7 @@ import lombok.*;
 @AllArgsConstructor
 public class OrderItem {
 	@Id
-	@SequenceGenerator(name = "ORDERITEM_ORDERITEM_NO_SEQ", sequenceName = "ORDERITEM_ORDERITEM_NO_SEQ", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ORDERITEM_ORDERITEM_NO_SEQ")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(length = 20)
 	private Long id;
 	@Column(length = 10)
