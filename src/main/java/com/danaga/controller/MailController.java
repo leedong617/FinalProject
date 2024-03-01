249b1c6d3aa9a1eaf7b749039c6bdf7a827f1e15
leedong617 <leedong617@gmail.com>
2024-03-02 02:04:29 +0900

    ..

--- src/main/java/com/danaga/controller/MailController.java
+++ src/main/java/com/danaga/controller/MailController.java
@@ -4,13 +4,12 @@ package com.danaga.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.danaga.dao.OrderDao;
 import com.danaga.entity.Orders;
 import com.danaga.service.MailService;
-import com.danaga.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,56 +19,44 @@ public class MailController {
 
     private final MailService mailService;
     private final OrderDao orderDao;
-    // 회원가입 이메일 인증번호 발송
+
     @PostMapping("/emailauthentication")
-    public String joinMailSend(String mail){
-
-       int number = mailService.joinSendMail(mail);
-       // JSON 데이터 String변환
-       String num = "" + number;
-       
-       return num;
+    public ResponseEntity<String> joinMailSend(@RequestParam String mail) {
+        int number = mailService.joinSendMail(mail);
+        return ResponseEntity.ok(String.valueOf(number));
     }
-    // 임시 비밀번호 발급
+
     @PostMapping("/findpassemailauthentication")
-    public Map findPassMailSend(String mail) throws Exception{
-    	
-    	String randomPass = null;
-		randomPass = mailService.findPassSendMail(mail);
-		HashMap map=new HashMap<>();
-    	map.put("newPass", randomPass);
-    	
-    	return map;
-    }
-    // 회원 아이디 발송
-    @PostMapping("/findidemailauthentication")
-    public Map findidMailSend(String mail) throws Exception{
-    	
-    	String id = null;
-		id = mailService.findIdSendMail(mail);
-    	HashMap map=new HashMap<>();
-    	map.put("sendId", id);
-    	return map;
-    }
-    // 주문 번호 발송
-    @PostMapping("/sendOrderIdemailauthentication")
-    public Map sendOrderIdMailSend(String mail, @RequestParam String orderId) throws Exception{
-    	
-    	String id = null;
-    	id = mailService.findOrderIdSendMail(mail, orderId);
-    	HashMap map=new HashMap<>();
-    	map.put("orderId", id);
-    	if(orderId!=null) {
-    		
-    		Orders orders= orderDao.findById(Long.parseLong(orderId));
-    		if(orders.getMember().getRole().equals("Guest")) {
-    			orders.getMember().setEmail(null);
-    		}
-    		orderDao.save(orders);
-    		return map;
-    	}else {
-    		throw new Exception("주문번호가 없습니다.");
-    	}
+    public ResponseEntity<Map<String, String>> findPassMailSend(@RequestParam String mail) throws Exception {
+        String randomPass = mailService.findPassSendMail(mail);
+        Map<String, String> map = new HashMap<>();
+        map.put("newPass", randomPass);
+        return ResponseEntity.ok(map);
     }
 
+    @PostMapping("/findidemailauthentication")
+    public ResponseEntity<Map<String, String>> findidMailSend(@RequestParam String mail) throws Exception {
+        String id = mailService.findIdSendMail(mail);
+        Map<String, String> map = new HashMap<>();
+        map.put("sendId", id);
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/sendOrderIdemailauthentication")
+    public ResponseEntity<Map<String, String>> sendOrderIdMailSend(@RequestParam String mail, @RequestParam String orderId) throws Exception {
+        String id = mailService.findOrderIdSendMail(mail, orderId);
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", id);
+        
+        if (orderId != null) {
+            Orders orders = orderDao.findById(Long.parseLong(orderId));
+            if (orders.getMember().getRole().equals("Guest")) {
+                orders.getMember().setEmail(null);
+            }
+            orderDao.save(orders);
+            return ResponseEntity.ok(map);
+        } else {
+            throw new Exception("주문번호가 없습니다.");
+        }
+    }
 }
--- src/main/java/com/danaga/controller/MemberRestController.java
+++ src/main/java/com/danaga/controller/MemberRestController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,223 +41,210 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberRestController {
-	private final MemberService memberService;
-	private final MemberDao memberDao;
-	private final CartService cartService;
-	private final RecentViewService recentViewService;
-	//아이디 찾기
-	@PostMapping(value = "/findid-rest", produces = "application/json;charset=UTF-8")
-	public Map member_findid_action_rest(@RequestBody MemberFindDto memberFindDto, HttpSession session)
-			throws Exception {
-		HashMap map = new HashMap<>();
-		// MemberResponseDto memberResponseDto =
-		// MemberResponseDto.builder().userName(userName).password(password).build();
-		int result = 1;
+    private final MemberService memberService;
+    private final MemberDao memberDao;
+    private final CartService cartService;
+    private final RecentViewService recentViewService;
 
-		try {
-			memberService.getMemberBy(memberFindDto.getEmail());
-		} catch (MemberNotFoundException e) {
-			result = 0;
-			map.put("result", result);
-			map.put("msg", memberFindDto.getEmail() + "는 등록되지 않은 이메일입니다.");
-			return map;
-		}
+    @PostMapping(value = "/findid-rest", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Map<String, Object>> member_findid_action_rest(@RequestBody MemberFindDto memberFindDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 1;
 
-		map.put("result", result);
-		return map;
-	}
-	// 비밀번호 찾기
-	@PostMapping(value = "/findpass-rest", produces = "application/json;charset=UTF-8")
-	public Map member_findpass_action_rest(@RequestBody MemberFindDto memberFindDto, HttpSession session)
-			throws Exception {
-		HashMap map = new HashMap<>();
-		// MemberResponseDto memberResponseDto =
-		// MemberResponseDto.builder().userName(userName).password(password).build();
-		int result = 2;
+        try {
+            memberService.getMemberBy(memberFindDto.getEmail());
+        } catch (MemberNotFoundException e) {
+            result = 0;
+            map.put("result", result);
+            map.put("msg", memberFindDto.getEmail() + "는 등록되지 않은 이메일입니다.");
+            return ResponseEntity.ok(map);
+        }
 
-		try {
-			memberService.isMatchEmailByUserName(memberFindDto.getUserName(), memberFindDto.getEmail());
-		} catch (MemberNotFoundException e) {
-			result = 0;
-			map.put("result", result);
-			map.put("msg", memberFindDto.getUserName() + "는 존재하지 않는 아이디입니다.");
-			return map;
-		} catch (EmailMismatchException e) {
-			result = 1;
-			map.put("result", result);
-			map.put("msg", "해당 아이디에 등록된 이메일이 아닙니다.");
-			return map;
-		}
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
 
-		map.put("result", result);
-		return map;
-	}
-	// 로그인
-	@Transactional
-	@PostMapping(value = "/login-rest", produces = "application/json;charset=UTF-8")
-	public Map member_login_action_rest(@RequestBody MemberLoginDto memberLoginDto, HttpSession session)
-			throws Exception {
-		HashMap map = new HashMap<>();
-		// MemberResponseDto memberResponseDto =
-		// MemberResponseDto.builder().userName(userName).password(password).build();
-		int result = 2;
+    @PostMapping(value = "/findpass-rest", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Map<String, Object>> member_findpass_action_rest(@RequestBody MemberFindDto memberFindDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 2;
 
-		try {
-			memberService.login(memberLoginDto.getUserName(), memberLoginDto.getPassword());
-		} catch (MemberNotFoundException e) {
-			result = 0;
-			map.put("result", result);
-			map.put("member", memberLoginDto);
-			return map;
-		} catch (PasswordMismatchException e) {
-			result = 1;
-			map.put("result", result);
-			return map;
-		}
-		MemberResponseDto loginUser = memberService.getMemberBy(memberLoginDto.getUserName());
-		/********* 비회원으로 카트 담고 로그인시 회원 카트 DB insert **********/
-		List<CartDto> fUserCarts = (List<CartDto>) session.getAttribute("fUserCarts");
-		session.setAttribute("sUserId", loginUser.getUserName());
-		if (fUserCarts != null) {
-			// 로그인 + 세션 장바구니 존재
-			for (int i = 0; i < fUserCarts.size(); i++) {
-				Cart findCart = cartService.findCart(loginUser.getUserName(), fUserCarts.get(i).getOptionSetId());
-				if (findCart != null) {
-					if (findCart.getQty() + fUserCarts.get(i).getQty() > 5) {
-						fUserCarts.get(i).setQty(5 - findCart.getQty());
-					}
-				}
-				cartService.addCart(fUserCarts.get(i), loginUser.getUserName());
-			}
-			session.setAttribute("fUserCarts", null);
-			session.setAttribute("countCarts", cartService.countCarts(loginUser.getUserName()));
-		}
-		/***************************************************************************************/
-		/********* 비회원으로 최근 본 상품 로그인시 회원 정보에 insert **********/
-		if (session.getAttribute("recentviews") != null) {
-			List<Long> recentOptionSetIds = (List<Long>) session.getAttribute("recentviews");
-			for (Long optionSetId : recentOptionSetIds) {
-				recentViewService.addRecentView(
-						RecentViewDto.builder().optionSetId(optionSetId).memberId(loginUser.getId()).build());
-			}
-			session.removeAttribute("recentviews");
-		}
-		/***************************************************************************************/
-		if (loginUser.getRole().equals("Admin")) {
-			session.setAttribute("role", loginUser.getRole());
-		}
+        try {
+            memberService.isMatchEmailByUserName(memberFindDto.getUserName(), memberFindDto.getEmail());
+        } catch (MemberNotFoundException e) {
+            result = 0;
+            map.put("result", result);
+            map.put("msg", memberFindDto.getUserName() + "는 존재하지 않는 아이디입니다.");
+            return ResponseEntity.ok(map);
+        } catch (EmailMismatchException e) {
+            result = 1;
+            map.put("result", result);
+            map.put("msg", "해당 아이디에 등록된 이메일이 아닙니다.");
+            return ResponseEntity.ok(map);
+        }
 
-		map.put("result", result);
-		return map;
-	}
-	// 회원가입
-	@PostMapping
-	public Map member_join_action(@RequestBody MemberResponseDto memberResponseDto) throws Exception {
-		HashMap map = new HashMap<>();
-		int result = 5;
-		memberResponseDto.setRole("Member");
-		try {
-			memberService.joinMember(Member.toResponseEntity(memberResponseDto));
-		} catch (ExistedMemberByUserNameException e) {
-			result = 1;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getUserName() + "는 사용중인 아이디입니다.");
-			return map;
-		} catch (ExistedMemberByEmailException e) {
-			result = 2;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getEmail() + "는 사용중인 이메일입니다.");
-			return map;
-		} catch (ExistedMemberByPhoneNoException e) {
-			result = 3;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getPhoneNo() + "는 사용중인 번호입니다.");
-			return map;
-		} catch (ExistedMemberByNicknameException e) {
-			result = 4;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getNickname() + "는 사용중인 닉네임입니다.");
-			return map;
-		}
-		map.put("result", result);
-		return map;
-	}
-	// 카카오 회원을 사이트회원으로 전환
-	@Transactional
-	@PostMapping("/join-rest-kakao")
-	public Map member_join_action_kakao(@RequestBody MemberResponseDto memberResponseDto, HttpSession session) throws Exception {
-		HashMap map = new HashMap<>();
-		// MemberResponseDto memberResponseDto =
-		// MemberResponseDto.builder().userName(userName).password(password).build();
-		int result = 3;
-		try {
-			String sUserId = (String) session.getAttribute("sUserId");
-			Long sUserLongId = memberService.getMemberBy(sUserId).getId();
-			memberResponseDto.setId(sUserLongId);
-			MemberResponseDto dto = memberService.kakaoToMember(KakaoMemberUpdateDto.toDto(Member.toResponseEntity(memberResponseDto)));
-			// 카카오 통합시 이벤트 랜덤 포인트 지급
-			int point = dto.getGradePoint() + memberDao.randomPoint();
-			dto.setGradePoint(dto.getGradePoint() + memberDao.randomPoint());
-			memberService.updateGrade(Member.toResponseEntity(memberResponseDto), point);
-		} catch (ExistedMemberByUserNameException e) {
-			result = 1;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getUserName() + "는 사용중인 아이디입니다.");
-			return map;
-		} catch (ExistedMemberByNicknameException e) {
-			result = 2;
-			map.put("result", result);
-			map.put("msg", Member.toResponseEntity(memberResponseDto).getNickname() + "는 사용중인 닉네임입니다.");
-			return map;
-		}
-		map.put("result", result);
-		session.removeAttribute("role");
-		return map;
-	}
-	// 회원 정보 수정
-	@LoginCheck
-	@PutMapping(value = "/modify-action-rest", produces = "application/json;charset=UTF-8")
-	public Map member_modify_action(@RequestBody MemberUpdateDto memberUpdateDto, HttpSession session)
-			throws Exception {
-		HashMap map = new HashMap<>();
-		int result = 2;
-		try {
-			String sUserId = (String) session.getAttribute("sUserId");
-			Long sUserLongId = memberService.getMemberBy(sUserId).getId();
-			memberUpdateDto.setId(sUserLongId);
-			memberService.updateMember(memberUpdateDto);
-		} catch (ExistedMemberByNicknameException e) {
-			result = 1;
-			map.put("result", result);
-			map.put("msg", memberUpdateDto.getNickname() + "는 사용중인 닉네임입니다.");
-			return map;
-		}
-		map.put("result", result);
-		return map;
-	}
-	// 회원탈퇴
-	@LoginCheck
-	@DeleteMapping(value = "/delete-action-rest", produces = "application/json;charset=UTF-8")
-	public Map delete_action_rest(@RequestBody MemberLoginDto memberLoginDto, HttpSession session) throws Exception {
-		HashMap map = new HashMap<>();
-		int result = 2;
-		try {
-			String sUserId = (String) session.getAttribute("sUserId");
-			MemberResponseDto member = memberService.getMemberBy(sUserId);
-			if (member.getPassword().equals(memberLoginDto.getPassword())) {
-				memberService.deleteMember(member.getUserName());
-				session.invalidate();
-			} else {
-				throw new PasswordMismatchException("비밀번호가 일치하지않습니다.");
-			}
-		} catch (PasswordMismatchException e) {
-			result = 1;
-			map.put("result", result);
-			map.put("msg", "비밀번호가 일치하지않습니다.");
-			return map;
-		}
-		map.put("result", result);
-		return map;
-	}
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
 
+    @Transactional
+    @PostMapping(value = "/login-rest", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Map<String, Object>> member_login_action_rest(@RequestBody MemberLoginDto memberLoginDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 2;
+
+        try {
+            memberService.login(memberLoginDto.getUserName(), memberLoginDto.getPassword());
+        } catch (MemberNotFoundException e) {
+            result = 0;
+            map.put("result", result);
+            map.put("member", memberLoginDto);
+            return ResponseEntity.ok(map);
+        } catch (PasswordMismatchException e) {
+            result = 1;
+            map.put("result", result);
+            return ResponseEntity.ok(map);
+        }
+
+        MemberResponseDto loginUser = memberService.getMemberBy(memberLoginDto.getUserName());
+        List<CartDto> fUserCarts = (List<CartDto>) session.getAttribute("fUserCarts");
+        session.setAttribute("sUserId", loginUser.getUserName());
+
+        if (fUserCarts != null) {
+            for (int i = 0; i < fUserCarts.size(); i++) {
+                Cart findCart = cartService.findCart(loginUser.getUserName(), fUserCarts.get(i).getOptionSetId());
+                if (findCart != null) {
+                    if (findCart.getQty() + fUserCarts.get(i).getQty() > 5) {
+                        fUserCarts.get(i).setQty(5 - findCart.getQty());
+                    }
+                }
+                cartService.addCart(fUserCarts.get(i), loginUser.getUserName());
+            }
+            session.setAttribute("fUserCarts", null);
+            session.setAttribute("countCarts", cartService.countCarts(loginUser.getUserName()));
+        }
+
+        if (session.getAttribute("recentviews") != null) {
+            List<Long> recentOptionSetIds = (List<Long>) session.getAttribute("recentviews");
+            for (Long optionSetId : recentOptionSetIds) {
+                recentViewService.addRecentView(
+                        RecentViewDto.builder().optionSetId(optionSetId).memberId(loginUser.getId()).build());
+            }
+            session.removeAttribute("recentviews");
+        }
+
+        if (loginUser.getRole().equals("Admin")) {
+            session.setAttribute("role", loginUser.getRole());
+        }
+
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> member_join_action(@RequestBody MemberResponseDto memberResponseDto) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 5;
+        memberResponseDto.setRole("Member");
+
+        try {
+            memberService.joinMember(Member.toResponseEntity(memberResponseDto));
+        } catch (ExistedMemberByUserNameException e) {
+            result = 1;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getUserName() + "는 사용중인 아이디입니다.");
+            return ResponseEntity.ok(map);
+        } catch (ExistedMemberByEmailException e) {
+            result = 2;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getEmail() + "는 사용중인 이메일입니다.");
+            return ResponseEntity.ok(map);
+        } catch (ExistedMemberByPhoneNoException e) {
+            result = 3;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getPhoneNo() + "는 사용중인 번호입니다.");
+            return ResponseEntity.ok(map);
+        } catch (ExistedMemberByNicknameException e) {
+            result = 4;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getNickname() + "는 사용중인 닉네임입니다.");
+            return ResponseEntity.ok(map);
+        }
+
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
+
+    @Transactional
+    @PostMapping("/join-rest-kakao")
+    public ResponseEntity<Map<String, Object>> member_join_action_kakao(@RequestBody MemberResponseDto memberResponseDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 3;
+        try {
+            String sUserId = (String) session.getAttribute("sUserId");
+            Long sUserLongId = memberService.getMemberBy(sUserId).getId();
+            memberResponseDto.setId(sUserLongId);
+            MemberResponseDto dto = memberService.kakaoToMember(KakaoMemberUpdateDto.toDto(Member.toResponseEntity(memberResponseDto)));
+            int point = dto.getGradePoint() + memberDao.randomPoint();
+            dto.setGradePoint(dto.getGradePoint() + memberDao.randomPoint());
+            memberService.updateGrade(Member.toResponseEntity(memberResponseDto), point);
+        } catch (ExistedMemberByUserNameException e) {
+            result = 1;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getUserName() + "는 사용중인 아이디입니다.");
+            return ResponseEntity.ok(map);
+        } catch (ExistedMemberByNicknameException e) {
+            result = 2;
+            map.put("result", result);
+            map.put("msg", Member.toResponseEntity(memberResponseDto).getNickname() + "는 사용중인 닉네임입니다.");
+            return ResponseEntity.ok(map);
+        }
+        map.put("result", result);
+        session.removeAttribute("role");
+        return ResponseEntity.ok(map);
+    }
+
+    @LoginCheck
+    @PutMapping(value = "/modify-action-rest", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Map<String, Object>> member_modify_action(@RequestBody MemberUpdateDto memberUpdateDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 2;
+        try {
+            String sUserId = (String) session.getAttribute("sUserId");
+            Long sUserLongId = memberService.getMemberBy(sUserId).getId();
+            memberUpdateDto.setId(sUserLongId);
+            memberService.updateMember(memberUpdateDto);
+        } catch (ExistedMemberByNicknameException e) {
+            result = 1;
+            map.put("result", result);
+            map.put("msg", memberUpdateDto.getNickname() + "는 사용중인 닉네임입니다.");
+            return ResponseEntity.ok(map);
+        }
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
+
+    @LoginCheck
+    @DeleteMapping(value = "/delete-action-rest", produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Map<String, Object>> delete_action_rest(@RequestBody MemberLoginDto memberLoginDto, HttpSession session) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        int result = 2;
+        try {
+            String sUserId = (String) session.getAttribute("sUserId");
+            MemberResponseDto member = memberService.getMemberBy(sUserId);
+            if (member.getPassword().equals(memberLoginDto.getPassword())) {
+                memberService.deleteMember(member.getUserName());
+                session.invalidate();
+            } else {
+                throw new PasswordMismatchException("비밀번호가 일치하지않습니다.");
+            }
+        } catch (PasswordMismatchException e) {
+            result = 1;
+            map.put("result", result);
+            map.put("msg", "비밀번호가 일치하지않습니다.");
+            return ResponseEntity.ok(map);
+        }
+        map.put("result", result);
+        return ResponseEntity.ok(map);
+    }
 }
